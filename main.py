ae4f1b92b8e42db6deef15a3bdafccc0e30185b4
Zoul Aimi <82052598+mdzoul@users.noreply.github.com>
2022-09-29 08:23:51 +0000

    Added art and updated interface

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # Calculator
+import time
+from art import logo
+from replit import clear
 
 # Add
 def add(n1, n2):
@@ -16,29 +19,46 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1 / n2
 
+# Calculating
+def calculate():
+    clear()
+    logo()
+    print("Calculating.")
+    time.sleep(0.5)
+    print("Calculating..")
+    time.sleep(0.5)
+    print("Calculating...")
+    time.sleep(0.5)
+    clear()
+
 # Operations dictionary
 operations = {
     "+": add,
     "-": subtract,
-    "*": multiply,
+    "x": multiply,
     "/": divide,
 }
 
 def calculator():
-    
+    logo()
     num1 = float(input("What's the first number? "))
+    symbols = []
     for symbol in operations:
-        print(symbol)
+        symbols += symbol
+    print(' | '.join(symbols))
     
     end_calculation = False
     while not end_calculation:    
         operations_symbol = input("Pick an operation: ")
         num2 = float(input("What's the next number? "))
         answer = operations[operations_symbol](num1, num2)
-        
+
+        calculate()
+
+        logo()
         print(f"{num1} {operations_symbol} {num2} = {answer}")
     
-        to_cont = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+        to_cont = input(f"---\nType 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
         if to_cont == 'y':
             num1 = answer
         else:
